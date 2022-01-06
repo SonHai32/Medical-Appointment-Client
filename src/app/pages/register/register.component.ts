87f75806e9c4667fdd417d364db83a811ff0dd45
hailam32 <hailam32.dev@gmail.com>
2022-01-06 23:55:44 +0700

    update login

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  validateForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  initForm() {
+    this.validateForm = this.fb.group({
+      username: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(64),
+      ]),
+      password: new FormControl(null, [Validators.required]),
+      fullname: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(50),
+      ]),
+      phoneNumber: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(10),
+      ]),
+      emailAddress: new FormControl(null, [
+        Validators.required,
+        Validators.email,
+      ]),
+    });
   }
-
 }
