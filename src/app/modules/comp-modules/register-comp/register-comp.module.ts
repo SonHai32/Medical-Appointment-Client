42b696805fc61b2890453bfdbc0a85c7efa0f278
hailam32 <hailam32.dev@gmail.com>
2022-01-06 23:55:44 +0700

    Update login

--- src/app/modules/comp-modules/register-comp/register-comp.module.ts
+++ src/app/modules/comp-modules/register-comp/register-comp.module.ts
@@ -1,12 +1,29 @@
+import { RegisterComponent } from './../../../pages/register/register.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { IconsProviderModule } from '../../core-modules/icons-provider.module';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 
 @NgModule({
-  declarations: [],
+  declarations: [RegisterComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    NzGridModule,
+    NzTypographyModule,
+    NzInputModule,
+    IconsProviderModule,
+    FormsModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzButtonModule,
+    NzCheckboxModule,
+  ],
+  exports: [RegisterComponent],
 })
-export class RegisterCompModule { }
+export class RegisterCompModule {}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  validateForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm() {
+    this.validateForm = this.fb.group({
+      username: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(64),
+      ]),
+      password: new FormControl(null, [Validators.required]),
+    });
+  }
+
+  getErrorTooltips(controlName: 'username' | 'password'): string {
+    switch (controlName) {
+      case 'username':
+        if (this.validateForm.controls[controlName].hasError('required')) {
+          return 'Vui lòng nhập tên tài khoản';
+        } else return '';
+      case 'password':
+        if (this.validateForm.controls[controlName].hasError('required')) {
+          return 'Vui lòng nhập mật khẩu';
+        } else return '';
+      default:
+        return '';
+    }
+  }
+
+  formSumbit() {
+    if (this.validateForm.valid) {
+      const username = this.validateForm.controls['username'].value;
+      const password = this.validateForm.controls['password'].value;
+
+      this.login(username, password);
+    } else {
+      this.refreshForm();
+    }
+  }
+
+  refreshForm() {
+    Object.values(this.validateForm.controls).forEach((c) => {
+      if (c.invalid) {
+        c.markAsDirty();
+        c.updateValueAndValidity();
+      }
+    });
+  }
+
+  login(username: string, password: string) {}
 }
